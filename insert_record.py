30c87bf8c8fc18a79d340f61464d717a55e3556d
bluepost59 <kaput.kat@gmail.com>
2021-12-14 22:50:45 +0900

    簡単な挿入

--- insert_record.py
+++ insert_record.py
@@ -1,1 +1,15 @@
 import sqlite3
+import os
+
+db_path = "db/main.db"
+
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
+
+if not(os.path.exists(db_path)):
+    cursor.execute("create table todo(title, detail);")
+
+cursor.execute("insert into todo values (?,?)", ("散歩", "ジャパンに買い物に行く"))
+
+cursor.execute("select * from todo")
+print(cursor.fetchall())
