fd159ee9ece7d876380df87a3066d041159166ac
bluepost59 <kaput.kat@gmail.com>
2021-12-16 15:50:52 +0900

    named styleの例も追加

--- insert_record.py
+++ insert_record.py
@@ -8,7 +8,12 @@ cursor = conn.cursor()
 
 # cursor.execute("create table todo(id integer, title text, detail text);")
 
+# qmark style
 cursor.execute("insert into todo values (?,?,?)", (5, "散歩", "ジャパンに買い物に行く"))
+
+# named style
+cursor.execute("insert into todo values (:id,:title,:detail)",
+               {"id": 3, "title": "買い物", "detail": "ヨドバシカメラで買い物する"})
 
 conn.commit()
 
